d2e66293eb899ca52ec505ddd2bcb8c9d82c2169
xirhxq <373836275@qq.com>
2023-06-19 16:44:04 +0800

    restructure interacts with targets pos; add targetCheck & targetReal

--- src/Classifier.py
+++ src/Classifier.py
@@ -7,30 +7,40 @@ import numpy as np
 class Classifier:
     def __init__(self):
         self.targets = []
-        self.target_cnts = []
+        self.targetsCnt = []
+        self.targetsCheck = []
+        self.targetsReal = []
         self.threshold = 0.5
+
+    def newTarget(self, pos):
+        self.targets.append(pos)
+        self.targetsCnt.append(1)
+        self.targetsCheck.append(False)
+        self.targetsReal.append(False)
+
+    def updateTarget(self, ind, pos):
+        self.targets[ind] = pos
+        self.targetsCnt[ind] += 1
+        if self.targetsCnt[ind] > 200:
+            self.targetsCheck[ind] = True
+            self.targetsReal[ind] = True
+
+    def firstNotChecked(self):
+        return self.targetsCheck.index(False)
 
     def newPos(self, x, y, z):
         if len(self.targets) == 0:
-            self.targets.append([x, y, z])
-            self.target_cnts.append(1)
+            self.newTarget([x, y, z])
         else:
-            # calculate the distance between the new position and all the known targets
             distances = []
             for target in self.targets:
                 distances.append(math.sqrt((x - target[0])**2 + (y - target[1])**2 + (z - target[2])**2))
             distances = np.array(distances)
-            # if the distance is less than 100m of any known target, then classify it as that target
             if np.any(distances < self.threshold):
-                # find the index of the minimum distance
                 index = np.argmin(distances)
-                # update the target position
-                self.targets[index] = [x, y, z]
-                self.target_cnts[index] += 1
-            # if the distance is greater than 100m of all known targets, then classify it as a new type of target
+                self.updateTarget(index, [x, y, z])
             else:
-                self.targets.append([x, y, z])
-                self.target_cnts.append(1)
+                self.newTarget([x, y, z])
 
     def targetsList(self):
         return self.targets
@@ -38,4 +48,4 @@ class Classifier:
     def outputTargets(self):
         outputT = self.targets
         for ind, t in enumerate(outputT):
-            print(f'[{ind + 1}] [{self.target_cnts[ind]}] @ {t[0]:.2f}, {t[1]:.2f}, {t[2]:.2f}')
+            print(f'[{ind + 1}] [{self.targetsCnt[ind]}] @ {t[0]:.2f}, {t[1]:.2f}, {t[2]:.2f}')
