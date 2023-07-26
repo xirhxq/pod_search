d6dc0bdfe5e7e6f789ac300f4cff101fd5210831
xirhxq_on_nx_1 <373836275@qq.com>
2023-07-26 19:02:45 +0800

    decrease threshold 100 -> 60

--- src/Classifier.py
+++ src/Classifier.py
@@ -14,7 +14,7 @@ class Classifier:
         self.threshold = 2
 
         self.checkThreshold = 10
-        self.targetThreshold = 100
+        self.targetThreshold = 60
 
     def newTarget(self, pos=[0, 0, 0]):
         self.targets.append(pos)
