988b561bac791dc337d19f7ea99507e6ab9ec5b8
jario <jariof@foxmail.com>
2023-11-30 22:32:18 +0800

    add TimeBuffer.py while remove it in Transformer.py; add filterpy==1.4.5 in requirements.txt

---
+++ src/TimeBuffer.py
@@ -0,0 +1,68 @@
+from collections import deque
+import rospy
+from std_msgs.msg import Float32
+
+
+class TimeBuffer:
+    def __init__(self, name='Buffer'):
+        self.buffer = deque()
+        self.name = name
+        self.maxAge = 0.4
+
+        self.preT = None
+        self.preMsg = None
+
+    @property
+    def empty(self):
+        return not self.buffer
+
+    def addMessage(self, msg):
+        time = rospy.Time.now()
+        print(f'{time.to_sec() = }')
+        self.buffer.append((time, msg))
+
+    def getData(self, x):
+        if hasattr(x, 'data'):
+            return x.data
+        else:
+            return x
+
+    def getMessage(self, time):
+        if not self.buffer:
+            return None
+
+        currentTime = rospy.Time.now()
+        targetTime = currentTime - rospy.Duration.from_sec(self.maxAge)
+        
+        while self.buffer and self.buffer[0][0] < targetTime:
+            self.preT = self.buffer[0][0]
+            self.preMsg = self.buffer[0][1]
+            self.buffer.popleft()
+
+        if self.preT == None or self.preMsg == None:
+            return None
+
+        if not (self.preT <= targetTime <= self.buffer[0][0]):
+            raise AssertionError(f'Buffer not right {self.preT.to_sec():.3f} -- {targetTime.to_sec():3f} -- {self.buffer[0][0]:.3f}')
+
+        t1 = self.preT.to_sec()
+        t2 = self.buffer[0][0].to_sec()
+        val1 = self.getData(self.preMsg)
+        val2 = self.getData(self.buffer[0][1])
+
+        ret = Float32(data=val1 + (val2 - val1) * (targetTime.to_sec() - t1) / (t2 - t1))
+        return ret
+
+        return self.buffer[0][1]
+
+    def getMessageNoDelay(self):
+        if not self.buffer:
+            return None
+        return self.buffer[-1][1]
+
+    def outputBuffer(self):
+        print(self.name + ': [')
+        t = rospy.Time.now()
+        for b in self.buffer:
+            print('-', (t - b[0]).to_sec(), b[1])
+        print(']')
--- src/Transformer.py
+++ src/Transformer.py
@@ -2,7 +2,6 @@
 
 import argparse
 import time
-from collections import deque
 from math import degrees, radians
 from signal import signal, SIGINT
 import numpy as np
@@ -17,6 +16,7 @@ from std_msgs.msg import Int8, Float32, Float64MultiArray, MultiArrayDimension
 
 from Classifier import Classifier
 from DataLogger import DataLogger
+from TimeBuffer import TimeBuffer
 from QuaternionBuffer import QuaternionBuffer
 from Utils import *
 import PodParas
@@ -29,70 +29,6 @@ def signal_handler(sig, frame):
 
 
 signal(SIGINT, signal_handler)
-
-
-class TimeBuffer:
-    def __init__(self, name='Buffer'):
-        self.buffer = deque()
-        self.name = name
-        self.maxAge = 0.4
-
-        self.preT = None
-        self.preMsg = None
-
-    @property
-    def empty(self):
-        return not self.buffer
-
-    def addMessage(self, msg):
-        time = rospy.Time.now()
-        self.buffer.append((time, msg))
-
-    def getData(self, x):
-        if hasattr(x, 'data'):
-            return x.data
-        else:
-            return x
-
-    def getMessage(self, time):
-        if not self.buffer:
-            return None
-
-        currentTime = rospy.Time.now()
-        targetTime = currentTime - rospy.Duration.from_sec(self.maxAge)
-        
-        while self.buffer and self.buffer[0][0] < targetTime:
-            self.preT = self.buffer[0][0]
-            self.preMsg = self.buffer[0][1]
-            self.buffer.popleft()
-
-        if self.preT == None or self.preMsg == None:
-            return None
-
-        if not (self.preT <= targetTime <= self.buffer[0][0]):
-            raise AssertionError(f'Buffer not right {self.preT.to_sec():.3f} -- {targetTime.to_sec():3f} -- {self.buffer[0][0]:.3f}')
-
-        t1 = self.preT.to_sec()
-        t2 = self.buffer[0][0].to_sec()
-        val1 = self.getData(self.preMsg)
-        val2 = self.getData(self.buffer[0][1])
-
-        ret = Float32(data=val1 + (val2 - val1) * (targetTime.to_sec() - t1) / (t2 - t1))
-        return ret
-
-        return self.buffer[0][1]
-
-    def getMessageNoDelay(self):
-        if not self.buffer:
-            return None
-        return self.buffer[-1][1]
-
-    def outputBuffer(self):
-        print(self.name + ': [')
-        t = rospy.Time.now()
-        for b in self.buffer:
-            print('-', (t - b[0]).to_sec(), b[1])
-        print(']')
 
 
 class Transformer:
