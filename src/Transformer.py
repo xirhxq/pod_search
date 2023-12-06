611b9fc58e70eb6456822b3d3c79559e49c64126
jario <jariof@foxmail.com>
2023-12-06 21:40:47 +0800

    turn vars into @property vars(to be decaprecated!!!)

--- src/Transformer.py
+++ src/Transformer.py
@@ -2,6 +2,7 @@
 
 import argparse
 import time
+import subprocess
 from math import degrees, radians
 from signal import signal, SIGINT
 import numpy as np
@@ -22,54 +23,17 @@ from Utils import *
 import PodParas
 from LocatingEKF import LocatingEKF
 
-
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    exit(0)
-
-
-signal(SIGINT, signal_handler)
-
-
 class Transformer:
     def __init__(self, args):
         self.args = args
         print(YELLOW + 'ARGS: ', self.args, RESET)
 
-        if self.args.cali:
-            import rospkg
-            pre = rospkg.RosPack().get_path('pod_search')
-            self.caliLog = DataLogger(pre, 'cali.csv')
-
-            variable_info = [
-                ('rosTime', 'double'),
-                ('podRoll', 'double'),
-                ('podRollDelayed', 'double'),
-                ("podPitch", "double"),
-                ("podPitchDelayed", "double"),
-                ("podYaw", "double"),
-                ("podYawDelayed", "double"),
-                ("podHfov", "double"),
-                ("podHfovDelayed", "double"),
-                ("podVfov", "double"),
-                ("podVfovDelayed", "double"),
-                ('pixelX', 'double'),
-                ('pixelY', 'double'),
-                ('cameraYaw', 'double'),
-                ('cameraPitch', 'double'),
-                ('uavQuat[4]', 'list'),
-                ('uavQuatDelayed[4]', 'list'),
-                ('uavEuler[3]', 'list'),
-                ('selfPos[3]', 'list'),
-                ('targetPosAbs[3]', 'list'),
-                ('targetId', 'int')
-            ]
-            self.caliLog.initialize(variable_info)
-            print(self.caliLog.variable_names) 
+        import rospkg
+        pre = rospkg.RosPack().get_path('pod_search')
+        self.ekfLog = DataLogger(pre, 'ekf.csv')
+        self.ekfLog.initialize([("usvEKFx[6][1]", "matrix")])
 
         if self.args.log:
-            import rospkg
-            pre = rospkg.RosPack().get_path('pod_search')
             self.dtlg = DataLogger(pre, "data.csv")
 
             self.targetsAvailable = 10
@@ -97,7 +61,6 @@ class Transformer:
             print(self.dtlg.variable_names)
 
         self.h = 10
-        self.a = self.h / 100 * 3000
         self.selfPos = np.array([0, 0, self.h])
         
         self.dockENU = np.array([20, 225, 0])
@@ -112,6 +75,13 @@ class Transformer:
         self.uavQuatBuffer = QuaternionBuffer('IMU Buffer')
         self.podDelay = 0.4
 
+        self.cameraAzimuth = None
+        self.cameraElevation = None
+
+        self.uavRollDeg = 0
+        self.uavPitchDeg = 0
+        self.uavYawDeg = 180
+
         self.uavName = 'suav'
         self.podName = 'pod'
         self.osdkName = 'dji_osdk_ros'
@@ -125,7 +95,7 @@ class Transformer:
         rospy.Subscriber(self.uavName + '/' + self.podName + '/yaw', Float32, self.yawCallback)
         rospy.Subscriber(self.uavName + '/' + self.podName + '/hfov', Float32, self.hfovCallback)
 
-        rospy.Subscriber(self.uavName + '/' + self.podName + '/laserRange', Float32, self.laserRangeCallback)
+        rospy.Subscriber(self.uavName + '/' + self.podName + '/laserRange', Float32, self.laserRangeCallback)    
 
         rospy.Subscriber(self.uavName + '/' + self.podName + '/vessel_det', TargetsInFrame, self.vesselDetectionCallback, queue_size=1)
         rospy.Subscriber(self.uavName + '/' + self.podName + '/usv_detection', TargetsInFrame, self.usvDetectionCallback, queue_size=1)
@@ -156,7 +126,7 @@ class Transformer:
             print(f'{GREEN}With rB2GB on{RESET}')
         self.rB2GB = R.from_euler('zyx', [self.yawB2GB, self.pitchB2GB, self.rollB2GB], degrees=True)
 
-        self.ekfDict = {}
+        self.usvEKF = LocatingEKF(initialT=self.getTimeNow())
     
     def getTimeNow(self):
         return rospy.Time.now().to_sec() - self.startTime
@@ -173,39 +143,110 @@ class Transformer:
     def rollCallback(self, msg):
         self.podRollBuffer.addMessage(msg)
 
+    @property
+    def podRoll(self):
+        return self.podRollBuffer.getMessage(self.podDelay)
+
     def pitchCallback(self, msg):
         self.podPitchBuffer.addMessage(msg)
+
+    @property
+    def podPitch(self):
+        return self.podPitchBuffer.getMessage(self.podDelay)
 
     def yawCallback(self, msg):
         self.podYawBuffer.addMessage(msg)
 
+    @property
+    def podYaw(self):
+        return self.podYawBuffer.getMessage(self.podDelay)
+
     def hfovCallback(self, msg):
         self.podHfovBuffer.addMessage(msg)
+
+    @property
+    def podHfov(self):
+        return self.podHfovBuffer.getMessage(self.podDelay)
+
+    @property
+    def podVfov(self):
+        return  PodParas.getVFovFromHFov(self.podHfov)
 
     def laserRangeCallback(self, msg):
         self.podLaserRangeBuffer.addMessage(msg)
 
+    @property
+    def podLaserRange(self):
+        return self.podLaserRangeBuffer.getMessage(self.podDelay)
+
+    @property
+    def rB2P(self):
+        try: 
+            rPodRoll = R.from_euler('x', self.podRoll, degrees=True)
+            rPodYaw = R.from_euler('z', self.podYaw, degrees=True)
+            rPodPitch = R.from_euler('y', self.podPitch, degrees=True)
+            rGB2P = rPodYaw * rPodRoll * rPodPitch
+            return rGB2P.as_matrix()
+        except:
+            return None
+
+    @property
+    def rP2B(self):
+        try:
+            return self.rB2P.T
+        except:
+            return None
+
+    @property
+    def rI2B(self):
+        try:
+            rUavRoll = R.from_euler('x', self.uavRollDeg, degrees=True)
+            rUavPitch = R.from_euler('y', self.uavPitchDeg, degrees=True)
+            rUavYaw = R.from_euler('z', self.uavYawDeg, degrees=True)
+            rI2B = rUavYaw * rUavRoll * rUavPitch
+            return rI2B.as_matrix()
+        except:
+            return None
+
+    @property
+    def rB2I(self):
+        try:
+            return self.rI2B.T
+        except:
+            return None
+
     def vesselDetectionCallback(self, msg):
         for target in msg.targets:
             if target.category_id != 100:
-                self.transform(target.cx, target.cy, target.category, target.category_id, target.score)
+                px = (target.cx - 0.5) * 2
+                py = (target.cy - 0.5) * 2
+                try:
+                    self.cameraAzimuth = -np.degrees(np.arctan(np.tan(np.radians(self.podHfov) / 2) * px))
+                    self.cameraElevation = np.degrees(np.arctan(np.tan(np.radians(self.podVfov) / 2) * py))
+                    trackData = Float64MultiArray(data=[self.cameraElevation + self.podPitch, self.cameraAzimuth + self.podYaw, self.podHfov * target.w / 0.15, 20])
+                    trackData.layout.dim = [MultiArrayDimension(label=target.category)]
+                    self.trackPub.publish(trackData)
+                except:
+                    pass
+                # self.transform(target.cx, target.cy, target.category, target.category_id, target.score)
 
     def usvDetectionCallback(self, msg):
+        pass
+        self.cameraAzimuth = None
+        self.cameraElevation = None
         for target in msg.targets:
-            self.transform(target.cx, target.cy, target.category, target.category_id, target.score)
+            px = (target.cx - 0.5) * 2
+            py = (target.cy - 0.5) * 2
+            try:
+                self.cameraAzimuth = -np.degrees(np.arctan(np.tan(np.radians(self.podHfov) / 2) * px))
+                self.cameraElevation = np.degrees(np.arctan(np.tan(np.radians(self.podVfov) / 2) * py))
+                trackData = Float64MultiArray(data=[self.cameraElevation + self.podPitch, self.cameraAzimuth + self.podYaw, self.podHfov * target.w / 0.2, 20])
+                trackData.layout.dim = [MultiArrayDimension(label=target.category)]
+                self.trackPub.publish(trackData)
+            except:
+                pass
 
     def calTarget(self, pixelX, pixelY, category, categoryID):
-        timeDiff = self.podDelay
-        try:
-            podHfov = self.podHfovBuffer.getMessage(timeDiff).data
-            podYaw = self.podYawBuffer.getMessage(timeDiff).data
-            podPitch = self.podPitchBuffer.getMessage(timeDiff).data
-            podRoll = self.podRollBuffer.getMessage(timeDiff).data
-            podLaserRange = self.podLaserRangeBuffer.getMessage(timeDiff).data
-        except Exception as e:
-            print(e)
-            return
-
         try: 
             uavQuat = self.uavQuatBuffer.getMessage()
         except Exception as e:
@@ -226,8 +267,13 @@ class Transformer:
         rGB2P = rPodYaw * rPodRoll * rPodPitch
         rI2B = R.from_quat(uavQuat)
         rI2P = rI2B * self.rB2GB * rGB2P
+        
+        trackData = Float64MultiArray(data=[cameraElevation + podPitch, cameraAzimuth + podYaw, podHfov, 10])
+        trackData.layout.dim = [MultiArrayDimension(label=category)]
+        self.trackPub.publish(trackData)
 
         if podLaserRange < 10 or podLaserRange > 3000:
+            # podLaserRange = 200.0
             return None
         
         if categoryID not in self.ekfDict:
@@ -237,9 +283,6 @@ class Transformer:
         self.ekfDict[categoryID].newFrame(self.getTimeNow(), Z, self.selfPos.reshape(3, 1), rGB2P.as_matrix().T, rI2B.as_matrix().T)
         realTargetAbsI = self.ekfDict[categoryID].ekf.x[0:3]
 
-        trackData = Float64MultiArray(data=[cameraElevation + podPitch, cameraAzimuth + podYaw, podHfov, podPitch])
-        trackData.layout.dim = [MultiArrayDimension(label=category)]
-        self.trackPub.publish(trackData)
 
 
         if self.args.debug:
@@ -289,14 +332,12 @@ class Transformer:
         realTargetAbs = self.calTarget(pixelX, pixelY, category, categoryID)
         if realTargetAbs is None:
             return
-        if not self.outOfBound(*realTargetAbs):
-            # self.clsfy.newPos(*realTargetAbs)
-            if category == 'boat' or category == 'usv':
-                if self.searchState == 6 or self.searchState == 4:
-                    self.clsfy.updateTarget(categoryID, list(realTargetAbs), score)
-            elif category == 'usv':
-                if self.searchState == 6:
-                    self.usvENU = realTargetAbs
+        if category == 'boat' or category == 'usv':
+            if self.searchState == 6 or self.searchState == 4:
+                self.clsfy.updateTarget(categoryID, list(realTargetAbs), score)
+        elif category == 'usv':
+            if self.searchState == 6:
+                self.usvENU = realTargetAbs
 
             # self.clsfy.outputTargets()
 
@@ -317,48 +358,6 @@ class Transformer:
 
         return podPitch, podYaw
 
-    def outOfBound(self, x, y, z):
-        if self.args.infBound:
-            return False
-        if x < 0 or x > self.a:
-            return True
-        if abs(y) > self.a / 2:
-            return True
-        return False
-
-    def log(self):
-        self.dtlg.log("rosTime", self.getTimeNow())
-        self.dtlg.log("podYaw", self.podYawBuffer.getMessageNoDelay().data)
-        self.dtlg.log("podYawDelayed", self.podYawBuffer.getMessage(self.podDelay).data)
-        self.dtlg.log("podPitch", self.podPitchBuffer.getMessageNoDelay().data)
-        self.dtlg.log("podPitchDelayed", self.podPitchBuffer.getMessage(self.podDelay).data)
-        self.dtlg.log("podRoll", self.podRollBuffer.getMessageNoDelay().data)
-        self.dtlg.log("podRollDelayed", self.podRollBuffer.getMessage(self.podDelay).data)
-        hFov = self.podHfovBuffer.getMessageNoDelay().data 
-        hFovDelayed = self.podHfovBuffer.getMessage(self.podDelay).data 
-        self.dtlg.log("podHfov", hFov)
-        self.dtlg.log("podHfovDelayed", hFovDelayed)
-        vFov = PodParas.getVFovFromHFov(hFov)
-        vFovDelayed = PodParas.getVFovFromHFov(hFovDelayed)
-        self.dtlg.log('podVfov', vFov)
-        self.dtlg.log('podVfovDelayed', vFovDelayed)
-        self.dtlg.log('selfPos', list(self.selfPos))
-        self.dtlg.log('uavQuatDelayed', list(self.uavQuatBuffer.getMessage()))
-        self.dtlg.log('uavQuat', list(self.uavQuatBuffer.getMessageNoDelay()))
-
-        t = self.clsfy.targets
-        tLen = len(t)
-        tCnt = self.clsfy.targetsCnt
-        for i in range(self.targetsAvailable):
-            if i < tLen:
-                self.dtlg.log(f'target{i}', t[i])
-                self.dtlg.log(f'targetCnt{i}', tCnt[i])
-            else:
-                self.dtlg.log(f'target{i}', [-1, -1, -1])
-                self.dtlg.log(f'targetCnt{i}', 0)
-
-        self.dtlg.newline()
-
     def ControlStateMachine(self):
         if self.searchState == 0 or self.searchState == 5:
             self.clsfy.clear()
@@ -368,50 +367,19 @@ class Transformer:
             self.ControlStateMachine()
 
             print('-' * 20)
-            # np.set_printoptions(precision=2)
-            # print(f'My pos: {self.selfPos}')
-            # print(f'RelImu: {R.from_quat(self.uavQuat).as_euler("zyx", degrees=True)}')
 
-            for key, ekf in self.ekfDict.items():
-                if ekf.t < self.getTimeNow() - 0.1:
-                    ekf.newFrame(self.getTimeNow(), np.array([0]), 0, 0, 0)
-                    self.clsfy.updateTarget(key, list(ekf.ekf.x[0:3]))
-
-            if self.args.log and \
-               not self.podYawBuffer.empty and \
-               not self.podPitchBuffer.empty and \
-               not self.podRollBuffer.empty and \
-               not self.uavQuatBuffer.empty:
-                self.log()
-
-            t = self.clsfy.targets
-            tLen = len(t)
-            tScore = self.clsfy.targetsScore
-            tCnt = self.clsfy.targetsCnt
-            for i in range(tLen):
-                print(
-                    f'Target[{i}] @ [{t[i][0][0]:.2f}, {t[i][1][0]:.2f}, {t[i][2][0]:.2f}], '
-                    f'{YELLOW if tCnt[i] < 15 else GREEN}'
-                    f'{tCnt[i]} Frames{RESET}, '
-                    f'Score: {tScore[i]:.2f}'
-                    f'{RESET}'
-                )
-
-            streamInd = self.clsfy.lowestScoreIndex(threshold=5)
-            if streamInd is not None and 0 <= streamInd < len(t):
-                streamPitch, streamYaw = self.untransform(self.clsfy.targets[streamInd])
-                msg = Float64MultiArray(data=[streamPitch, streamYaw, streamInd])
-                self.streamPub.publish(msg)
-
-                if self.searchState == 6:
-                    realTargetRelUSVI = self.clsfy.targets[streamInd] - self.usvENU 
-                    realTargetRelUSVTheta = np.degrees(np.arctan2(*realTargetRelUSVI[[1, 0]]))
-                    self.usvTargetPub.publish(Pose2D(x=realTargetRelUSVI[0], y=realTargetRelUSVI[1], theta=realTargetRelUSVTheta)) 
+            if self.cameraAzimuth is None or self.cameraElevation is None:
+                newZ = np.array([[0.0], [0.0], [0.0], [self.selfPos[2]]])
+            else:
+                newZ = np.array([[self.podLaserRange], [self.cameraAzimuth], [self.cameraElevation], [self.selfPos[2]]])
             
-            dockPitch, dockYaw = self.untransform(self.dockENU)
-            self.dockPub.publish(Float64MultiArray(data=[1, dockPitch, dockYaw, 0]))
+            self.usvEKF.newFrame(self.getTimeNow(), newZ, self.selfPos.reshape(3, 1), self.rP2B, self.rB2I)
 
-            time.sleep(0.05)
+            print(f'{self.usvEKF.ekf.x = }')
+            self.ekfLog.log('usvEKFx', self.usvEKF.ekf.x)
+            self.ekfLog.newline()
+
+            time.sleep(0.01)
 
 
 if __name__ == '__main__':
@@ -419,13 +387,25 @@ if __name__ == '__main__':
     parser.add_argument('--log', help='turn on log or not', action="store_true")
     parser.add_argument('--debug', help='debug or not', action='store_true')
     parser.add_argument('--infBound', help='infinite bound', action='store_true')
-    parser.add_argument('--cali', help='calibration', action='store_true')
     parser.add_argument('--noB2GB', help='not adding rB2GB', action='store_true')
     parser.add_argument('--noImu', help='not using IMU', action='store_true')
+    parser.add_argument('--bag', help='rosbag record', action='store_false')
     args, unknown = parser.parse_known_args()
+
+    if args.bag:
+        command = 'rosbag record -a -x "/suav/pod/main_camera_images.*" -o track'
+        process = subprocess.Popen(command, shell=True)
+
+    def signal_handler(sig, frame):
+        print('You pressed Ctrl+C!')
+        if args.bag:
+            process.terminate()
+        exit(0)
+
+    signal(SIGINT, signal_handler)
     
     rospy.init_node('Transformer', anonymous=True)
     t = Transformer(args)
-    time.sleep(1)
+    time.sleep(3)
 
     t.spin()
